0ce85f535c21ccdfb14e1084fae3bf13be8cb62b
Suyash Srijan <suyash@blixcorp.com>
2013-07-15 21:56:42 +0530

    [Experimental] Start button behavior override
    The Start Screen will only open when you press the Start button on your
    keyboard. When you will press the Start button on the taskbar, it will
    open the All Apps view instead, thus turning the taskbar Start button
    into a permanent Start menu replacement while still retaining anytime
    access to the Start Screen by simply pressing the Start button on the
    keyboard.

--- mrem/mrem.cpp
+++ mrem/mrem.cpp
@@ -23,19 +23,66 @@
 #include <assert.h>
 #define VERSIONHELPERAPI FORCEINLINE BOOL
 
-HWND _iconHwnd; // Window handle to the start button
-HWND _iconParentHwnd; // Window handle to the start button parent
-HWND _tBarListHwnd; // Window handle to the taskbar app container
-HWND _mUIHwnd; // Window handle which has loaded windows.immersiveshell.serviceprovider.dll into memory
+HWND _iconHwnd; // Window handle of start button
+HWND _iconParentHwnd; // Window handle of start button parent
+HWND _tBarListHwnd; // Window handle of taskbar app container
+HWND _mUIHwnd; // Window handle of the window which has loaded windows.immersiveshell.serviceprovider.dll into memory
 RECT _tBarList; // Taskbar app container's RECT structure 
 RECT _icon; // Start button's RECT structure 
 DWORD _mUIThreadId; // Id of the thread which has loaded windows.immersiveshell.serviceprovider.dll into memory
 HANDLE _mUI; // Handle of the thread which has loaded windows.immersiveshell.serviceprovider.dll into memory
 long _newLength; // New length of the taskbar
 DWORD _expPID; // PID of explorer process
-HANDLE _eSThandle; // Handle to system tray window
+HANDLE _eSThandle; // Handle to explorer.exe
+HWND _explorerSysTray; // Window handle of the system tray
+HHOOK _keyHook; // Keyboard hook
+KBDLLHOOKSTRUCT _hookStruct; // Keyboard hook structure
+MSG _msg; // Temp message
+HKEY key = NULL; // Handle to registry key we want to modify
+LONG ret = ERROR_SUCCESS; // Value of ERROR_SUCCESS
+
+LRESULT WINAPI keyDownEvent(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if ((nCode == HC_ACTION) && ((wParam == VK_RWIN) || (wParam == WM_KEYDOWN))) // Windows key is pressed
+	{
+		ret = RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartPage"), 0, KEY_ALL_ACCESS, &key); // Try opening registry key
+
+		if (ret == ERROR_SUCCESS) // The key exists!
+
+		{
+			BYTE buf = MAX_PATH; // Set value of buffer
+			DWORD dwBufSize = sizeof(buf); // Set size of buffer
+			DWORD ENABLE_APPS_VIEW = 1; // Open Apps view when StartScreen is opened
+			DWORD DISABLE_APPS_VIEW = 0; // Don't open Apps view when Start Screen is opened
+			RegQueryValueEx(key, TEXT("MakeAllAppsDefault"), NULL, NULL, &buf, &dwBufSize); // Get value of MakeAllAppsDefault subkey
+
+			if (buf == 1) // Value is 1 (ON)
+			
+			{
+				RegSetValueEx(key, TEXT("MakeAllAppsDefault"), NULL, REG_DWORD, (const BYTE*) DISABLE_APPS_VIEW, sizeof(DISABLE_APPS_VIEW)); // Set value to 0 (OFF)
+			}
+
+			else // Value is 0 (OFF)
+
+			{
+				RegSetValueEx(key, TEXT("MakeAllAppsDefault"), NULL, REG_DWORD, (const BYTE*) ENABLE_APPS_VIEW, sizeof(ENABLE_APPS_VIEW)); // Set value to 1 (ON)
+			}
+		}
+
+		else // Error occured
+
+		{
+			std::wcout << "Error accessing registry. Error: " << GetLastError();
+		}
+
+		RegCloseKey(key); // Close the registry key
+
+	}
+	return CallNextHookEx(_keyHook, nCode, wParam, lParam); // Pass info to next hook procedure
+}
 
 int _tmain(int argc, _TCHAR* argv[])
+
 {
 	if (lstrcmpi(argv[1], _T("--killstart")) == 0) { // Kill start button? Sure
 
@@ -46,20 +93,6 @@ int _tmain(int argc, _TCHAR* argv[])
 		 std::wcout << "Trying to hide Start button"; // Tell user we are about to kill the start menu
 		_iconParentHwnd = FindWindow(_T("Shell_TrayWnd"), NULL); // Get the taskbar window handle
 		_iconHwnd = FindWindowEx(_iconParentHwnd, NULL, _T("Start"), NULL); // Get the start button handle
-
-		/*
-
-		SUPER BUGGY: I was trying to resize the taskbar so that the empty space left after removing the
-		start button gets utilized but the code below is really buggy and crashes the explorer process most of the time 
-		so it has been commented out while I am working on a fix.
-
-		_tBarListHwnd = FindWindowEx(_iconParentHwnd, NULL, _T("ReBarWindow32"), NULL);
-		GetWindowRect(_tBarListHwnd, &_tBarList);
-		GetWindowRect(_iconHwnd, &_icon);
-		_newLength = _icon.left + _icon.right;
-		MoveWindow(_tBarListHwnd, (_tBarList.left - _newLength), _tBarList.top, ((_tBarList.left + _tBarList.right) + _newLength), ((_tBarList.top + _tBarList.bottom) + _newLength), TRUE);
-
-		*/
 
 		LRESULT _iconHdl = SendMessage(_iconHwnd, WM_CLOSE, NULL, NULL); // Send a WM_CLOSE message to start button
 		signed int _reVal = static_cast<int>(_iconHdl); // Store the return value
@@ -97,46 +130,56 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 
-	else if (lstrcmpi(argv[1], _T("--sbopenav")) == 0) { // Open the All apps view when Start button on the keyboard is pressed? Sure
+	else if (lstrcmpi(argv[1], _T("--sbopenav")) == 0) { // Open the Start Screen only when Start button on the keyboard is pressed? Sure
+		
+		// Checking if you are on 8.1 or not
+		assert("\n\nStart button trick cannot be applied since you're not running Windows 8.1", IsWindows8_1OrGreater());
 
-		 /*
-		 
-		 Open the all apps view when start button on the keyboard is pressed
-		 To do that, we have to hook into the keyboard and capture the WM_KEYDOWN event and
-		 call CImmersiveLauncher::OnStartButtonPressed() which is found in twinui.dll and
-		 pass IMMERSIVELAUNCHERSHOWMETHOD and IMMERSIVELAUNCHERDISMISSMETHOD arguments. The function is 
-		 undocumented so I am studying the library to find out more details about the above parameters.
-		 
-		 */
+		// You're on 8.1, good
+		_keyHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)keyDownEvent, GetModuleHandle(NULL), 0); // Set keyboard hook
 
-		std::wcout << "\n\nWIP"; // Still working
-        std::getwchar();
+		while (GetMessage(&_msg, NULL, 0, 0)) // Loop in order to dispatch and recieve system messages
+		{
+			TranslateMessage(&_msg);
+			DispatchMessage(&_msg);
+		}
 	}
 
 	else if (lstrcmpi(argv[1], _T("--resexplr")) == 0) {
 
 		// Restart Explorer
 
-		HWND _explorerSysTray = FindWindow(TEXT("Shell_TrayWnd"), NULL); // Find the system tray window
-		GetWindowThreadProcessId(_explorerSysTray, &_expPID); // Get the explorer.exe PID
-		_eSThandle = OpenProcess(PROCESS_TERMINATE, FALSE, _expPID); // Set the PROCESS_TERMINATE flag
+		_explorerSysTray = FindWindow(TEXT("Shell_TrayWnd"), NULL); // Find the system tray window and store its handle
+		GetWindowThreadProcessId(_explorerSysTray, &_expPID); // Get the PID of the process (explorer.exe) which created the window 
+		_eSThandle = OpenProcess(PROCESS_TERMINATE, FALSE, _expPID); // Set the PROCESS_TERMINATE flag in explorer.exe process
 
-		if (_eSThandle)
+		if (_eSThandle) // Valid handle
 
 		{
 			TerminateProcess(_eSThandle, 0); // Terminate the explorer.exe process
+			Sleep(2000); // Take a nap
+			ShellExecute(NULL, NULL, TEXT("cmd.exe /c explorer.exe"), NULL, NULL, SW_HIDE); // Launch a brand sparkling new Explorer process (thanks to Shantanu (@zeusk))
 		}
 
-		Sleep(2000); // Take a nap
-		ShellExecute(NULL, NULL, TEXT("cmd.exe /c explorer.exe"), NULL, NULL, SW_HIDE); // Launch a brand sparkling new Explorer process (thanks to Shantanu (@zeusk)
+		else // Invalid handle
+		
+		{
+			std::wcout << "Invalid handle, looks like process has already been killed";
+			std::getwchar(); 
+		}
 	}
 	
 	else { // Invalid command-line arguments specified 
 
 		std::wcout << "Invalid command-line parameter \n\n Usage: \n --killstart : Remove start button \n --killmetro : Remove the Modern UI completely \n --sbopenav : Open the All Apps view only when the start button is pressed \n --resexplr : Restart the explorer process";
-                std::getwchar();
+        std::getwchar();
 		
 	}
-                return 0;
+
+	UnhookWindowsHookEx(_keyHook); // Unhook the keyboard hook
+	return 0; // Do I really need to explain what this does?
+
 }
+
+
 
